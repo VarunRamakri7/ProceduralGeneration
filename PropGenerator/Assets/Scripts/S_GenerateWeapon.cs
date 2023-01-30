80f7b60212395233fa46a39953b4a54479d7ce3d
Varun Ramakrishnan <ramakri7@purdue.edu>
2023-01-30 15:36:18 -0500

    Randomly choose type of gun to generate

--- PropGenerator/Assets/Scripts/S_GenerateWeapon.cs
+++ PropGenerator/Assets/Scripts/S_GenerateWeapon.cs
@@ -17,6 +17,7 @@ public class S_GenerateWeapon : S_Weapon
     // Start is called before the first frame update
     void Start()
     {
+        gunParent = this.gameObject;
         genType = Type.NONE;
     }
 
@@ -26,5 +27,30 @@ public class S_GenerateWeapon : S_Weapon
     public void GenerateGun()
     {
         genType = (Type)Random.Range(1, 3); // Choose a weapon type randomly
+
+        genType = Type.CROSSBOW;
+        Debug.Log("Random type: " + genType.ToString());
+
+        GameObject weapon = null;
+        switch(genType)
+        {
+            case Type.AR:
+                Debug.Log("AR");
+                weapon = generateAR.Generate();
+                break;
+            case Type.SNIPER:
+                Debug.Log("Sniper");
+                weapon = generateSniper.Generate();
+                break;
+            case Type.CROSSBOW:
+                Debug.Log("Crossbow");
+                weapon = generateCrossbow.Generate();
+                break;
+        }
+
+        weapon.transform.position = this.transform.position; // Set weapon position
+        weapon.transform.SetParent(gunParent.transform); // Set weapon parent
+
+        //Instantiate<GameObject>(weapon);
     }
 }
